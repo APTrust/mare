5617bc89cdb1e8ada4daa40b37a616ca1edf3739
Andrew Curley <andrew.curley@gmail.com>
2013-07-01 18:24:05 -0400

    Initial commit with MongoDB and puma

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Echoes::Application.config.secret_key_base = '732c2f574f6753810ca6cbfe8453a92e0df4337e09e4fd6c30adab306a4a685803b2c5220418e662e8c8aaa800b87723e50fcf1b99719cf31b6ca27e201b3b12'
