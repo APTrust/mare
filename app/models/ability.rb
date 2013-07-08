55a8dd5b903cdbc7201e5640fdfd4762e692c459
Andrew Curley <andrew.curley@gmail.com>
2013-07-08 05:02:03 -0400

    Add initial outline of users and role base authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :manage, :all
+    # Define abilities for the passed in user here.
+    user ||= User.new # guest user (not logged in)
+    if user.is? :superuser
+      can :manage, :all
+    elsif user.is? :institutional_admin
+      can :manage, DescriptionObject do |description_object|
+        description_object.insitution_id = user.insitution_id
       end
+    else
+      cannot :read, DescriptionObject
+    end
+
     #
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
