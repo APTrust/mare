de596906e4a26b17ad938a33a666b3f19351ab7a
Andrew Curley <andrew.curley@gmail.com>
2013-07-09 18:36:28 -0400

    Add phone_number to permitted attributes on UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user].permit(:name, :institution_id, :email, :role)
+      params[:user].permit(:name, :institution_id, :email, :role, :phone_number)
     end
 end
