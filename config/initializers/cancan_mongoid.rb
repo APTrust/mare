7bc343d403522eeab067f3cad59c9cb33b244e24
Andrew Curley <andrew.curley@gmail.com>
2013-07-10 02:14:18 -0400

    Remove unnecessary initializer now that I know I need to load gem 'mongoid' before gem 'cancan'

--- config/initializers/cancan_mongoid.rb
+++
@@ -1,7 +0,0 @@
-# Cancan 1.6.10 / Rails 4.0.0 / Mongoid 4.0.0 do not interact in some critical way
-# as to load the Cancan mongoid adapter.  I've registered this issue at:
-# https://github.com/ryanb/cancan/issues/896
-#
-# In the meantime, we will complete the necessary requirements in an initializer...
-require 'cancan/model_adapters/mongoid_adapter'
-include CanCan::ModelAdditions::ClassMethods
